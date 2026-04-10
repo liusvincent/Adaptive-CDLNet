3ae33d2af99ee7d19c2bccfe1d1fcc539bfabde4
Vincent Sugianto <79440920+liusvincent@users.noreply.github.com>
2026-04-09 22:15:12 -0400

    add AdaCDLNet_SM
    CDLNet using a single matrix form Ada-LISTA

--- model/net.py
+++ model/net.py
@@ -266,3 +266,120 @@ class FFDNet(DnCNN):
 		xhatp = F.pixel_shuffle(z, 2)
 		xhat  = unpad(xhatp, pad)
 		return xhat, noise_map
+
+#############################################
+#                   Extra                   #
+#############################################
+
+class AdaCDLNet_SM(nn.Module):
+    """ 
+        Convolutional Dictionary Learning Network with Ada-LISTA (Single Matrix)
+
+        Update step:
+        z{k+1} = ST(z{k} - D^T W{k}^T (D z{k}) - y, tau{k})
+
+        Where:
+        D    ...convolutional synthesis dictionary, shape: [M, C, P, P]
+        DT   ...convolutional analysis dictionary
+        W{k} ...learned parameter 
+    """
+    def __init__(self,
+                 K = 3,             # num. unrollings
+                 M = 64,            # num. filters in each filter bank operation
+                 P = 7,             # square filter side length
+                 s = 1,             # stride of convolutions
+                 C = 1,             # num. input channels
+                 t0 = 0,            # initial threshold
+                 adaptive = False,  # noise-adaptive thresholds
+                 init = True,       # False -> use power-method for weight init
+                 D = None):         # dictionary input
+        super(AdaCDLNet_SM, self).__init__()
+        
+        # -- OPERATOR INIT --
+        self.W = nn.ModuleList([nn.Conv2d(C, C, P, stride=s, padding=(P-1)//2, bias=False)  for _ in range(K)])
+        self.t = nn.Parameter(t0*torch.ones(K,2,M,1,1)) # learned thresholds
+
+        # set weights 
+        W_temp = torch.randn(C, C, P, P)
+        for k in range(K):
+            self.W[k].weight.data = W_temp.clone()
+
+        # Don't bother running code if initializing trained model from state-dict
+        if init:
+            print("Running power-method on initial dictionary...")
+            with torch.no_grad():
+                center = P // 2
+                for k in range(K):
+                    self.W[k].weight.zero_()
+                    for c in range(C):
+                        self.W[k].weight[c, c, center, center] = 1.0
+
+        # set parameters
+        self.K = K
+        self.M = M
+        self.P = P
+        self.s = s
+        self.t0 = t0
+        self.adaptive = adaptive
+
+    @torch.no_grad()
+    def project(self):
+        """ 
+        \ell_2 ball projection for filters, R_+ projection for thresholds
+        """
+        self.t.clamp_(0.0) 
+        for k in range(self.K):
+            self.W[k].weight.data = uball_project(self.W[k].weight.data)
+
+    def synthesis(self, z, D_filters):
+        """
+        x = D z
+        """
+        return F.conv_transpose2d(
+            z,
+            D_filters,
+            stride = self.s,
+            padding=(self.P-1)//2
+        )
+    
+    def analysis(self, x, D_filters):
+        """
+        z = D^T x
+        """
+        return F.conv2d(
+            x, 
+            D_filters,
+            stride = self.s,
+            padding=(self.P-1)//2
+        )
+
+    def forward(self, y, D_filters, sigma=None, mask=1):
+        """ AdaLISTA + D w/ noise-adaptive thresholds
+        """ 
+        yp, params, mask = pre_process(y, self.s, mask=mask)
+
+        # THRESHOLD SCALE-FACTOR c
+        c = 0 if sigma is None or not self.adaptive else sigma/255.0
+
+        # Ada-LISTA
+        
+        z = ST(self.analysis(self.W[0](yp), D_filters), self.t[0,:1] + c*self.t[0,1:2])
+        for k in range(1, self.K):
+            z = ST(z - self.analysis(self.W[k](mask * self.synthesis(z, D_filters) - yp), D_filters), self.t[k,:1] + c*self.t[k,1:2])
+
+        # DICTIONARY SYNTHESIS
+        xphat = self.synthesis(z, D_filters)
+        xhat  = post_process(xphat, params)
+        return xhat, z
+
+    def forward_generator(self, y, D_filters, sigma=None, mask=1):
+        """ same as forward but yeilds intermediate sparse codes
+        """
+        yp, params, mask = pre_process(y, self.s, mask=mask)
+        c = 0 if sigma is None or not self.adaptive else sigma/255.0
+        z = ST(self.analysis(self.W[0](yp), D_filters), self.t[0,:1] + c*self.t[0,1:2])
+        for k in range(1, self.K):
+            z = ST(z - self.analysis(self.W[k](mask * self.synthesis(z, D_filters) - yp), D_filters), self.t[k,:1] + c*self.t[k,1:2])
+        xphat = self.synthesis(z)
+        xhat  = post_process(xphat, params)
+        return xhat, z
